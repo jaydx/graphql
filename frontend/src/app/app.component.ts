bad26954c184333d11d53f6ed794f16f30d8502e
Jay <jaredglass-wdx@outlook.com>
2019-11-27 19:09:37 +0000

    search field

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -12,17 +12,35 @@ export class AppComponent implements OnInit {
   loading = true;
   id: string;
   types: Array<string>;
+  anyType = "Any"; 
+  selectedType: string;
+  searchPhrase: string;
 
   constructor(private apollo: Apollo) {
     this.id = '1';
+    this.selectedType = this.anyType; 
   }
 
   ngOnInit(): void {
     this.runQuery();
   }
 
-  onSelectTypeChange(selectedType : string){
-    console.log(selectedType);
+  onSelectTypeChange(selectedType: string){
+    console.log(`Selected Type: ${selectedType}`);
+    this.selectedType = selectedType;
+
+    this.searchCharacters();
+  }
+
+  onSearchChange(searchPhrase: string){
+    console.log(`Search: ${searchPhrase}`);
+    this.searchPhrase = searchPhrase;
+
+    this.searchCharacters();
+  }
+
+  searchCharacters() {
+    console.log(`Search: ${this.searchPhrase} for ${this.selectedType} starwarstype.`);
   }
 
   runQuery() {
